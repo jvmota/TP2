7c43a5f311db7a556c5bd3b27647b8b5adedfe2e
Jhon <Jhon@localhost.localdomain>
2019-06-14 10:49:11 -0300

    Comeco do tb do banco (falta gerar os sinais, tá montado)

--- TB_RegisterFile.v
+++ TB_RegisterFile.v
@@ -1,7 +1,40 @@
 module TB_RegisterFile();
 
-reg [31:0] I1, I2;
-reg [3:0] OP;
-wire [31:0] O;
+reg [1:0] F1, F2, RE;
+reg [31:0] D;
+reg E, C;
+wire [31:0] D1, D2;
+
+RegisterFile instancia_RF(
+	.RegEsc(RE),
+	.Fonte1(F1),
+	.Fonte2(F2),
+	.Dado(D),
+	.Esc(E),
+	.Clk(C),
+	.Dado1(D1),
+	.Dado2(D2)
+);
+
+initial
+begin
+	D = 32'h048fc24a;
+	I2 = 32'H00000004;
+	OP = 4'b1000;
+	#10
+	I1 = 32'h00000004;
+	I2 = 32'H00000002;
+	OP = 4'b0100;
+	#10
+	I1 = 32'h00000006;
+	I2 = 32'H00000004;
+	OP = 4'b0010;
+	#10
+	I1 = 32'h00000008;
+	I2 = 32'H00000002;
+	OP = 4'b0001;
+	#10
+	$finish;
+end
 
 endmodule
---
+++ TB_RegisterFile.v.bak
@@ -0,0 +1,7 @@
+module TB_RegisterFile();
+
+reg [31:0] I1, I2;
+reg [3:0] OP;
+wire [31:0] O;
+
+endmodule
